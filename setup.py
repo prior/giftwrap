30478f61ac8bd239c0fb3e7a7c2bed90ef6c6328
prior <prior@cracklabs.com>
2012-04-05 20:09:39 -0400

    update dependencies

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 
 setup(
     name='giftwrap',
-    version='1.2',
+    version='1.2.1',
     description='Python Api Wraping Toolset',
     author='Michael Prior',
     author_email='prior@cracklabs.com',
     url='https://github.com/prior/giftwrap',
-    download_url='https://github.com/prior/giftwrap/tarball/v1.2',
+    download_url='https://github.com/prior/giftwrap/tarball/v1.2.1',
     packages=['giftwrap'],
     install_requires=[]
 )
