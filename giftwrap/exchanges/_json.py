78f89569e053484abf155707498866e9597348e3
prior <prior@cracklabs.com>
2012-04-09 23:27:35 -0400

    more forgiving of emtpy json bodies

--- giftwrap/exchanges/_json.py
+++ giftwrap/exchanges/_json.py
@@ -6,7 +6,9 @@ class JsonExchange(Exchange):
     headers = { 'Content-Type':'application/json', 'Accept':'application/json' }
 
     def process_response(self, response): 
-        data = json.loads(response.text)
+        data = None
+        if response.text and response.text.strip():
+            data = json.loads(response.text)
         return self.process_data(data, response)
 
     def process_data(self, data, response): return NotImplementedError()
