839d592f630a64535c7c52dab5a042aab6cf968b
prior <prior@cracklabs.com>
2012-04-09 23:28:52 -0400

    dry version

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
+VERSION='1.3.1'
+
 setup(
     name='giftwrap',
-    version='1.3.1',
+    version=VERSION,
     description='Python Api Wraping Toolset',
     author='Michael Prior',
     author_email='prior@cracklabs.com',
     url='https://github.com/prior/giftwrap',
-    download_url='https://github.com/prior/giftwrap/tarball/v1.3.1',
+    download_url='https://github.com/prior/giftwrap/tarball/v%s'%VERSION,
     packages=['giftwrap'],
     install_requires=[]
 )
